0e0d998992f849c914f71c5604e6e0f0009327e0
Ahmed Madkour <ahmed.madkour@gmail.com>
2006-11-14 22:07:43 +0000

    1. changed the code to adhere to agreed upon coding standards. 2. Added public accessors functions.

--- src/persistence/Presist.h
+++ src/persistence/Presist.h
@@ -2,8 +2,8 @@
 This object is responsible for presisting and retrieving all the application data to and from files. 
 it references the baseClass.h */
 
-#ifndef Presist
-#define Presist
+#ifndef PERSIST_H
+#define PERSIST_H
 #include "baseclass.h"
 
 enum Mode
@@ -12,15 +12,19 @@ enum Mode
 	save = 2
 	}
 
-class Presist
+class Persist
   {
   public:
-    Presist(BaseClass);                // gets the BaseClass without a file open mode.  
-    Presist(BaseClass, Mode);          // gets the BaseClass and the mode of operation to be done with it. 
-  public:
-    ~Presist(void);                    // destructor ensures the file has been closed. 
+    Persist(BaseClass);                // gets the BaseClass without a file open mode.  
+    Persist(BaseClass, Mode);          // gets the BaseClass and the mode of operation to be done with it. 
+    ~Persist(void);                    // destructor ensures the file has been closed. 
+    BaseClass load(string className);  // public member function to load class. 
+	bool save (BaseClass);             // public member function to save class. 
+
   private:
     string className;                  //holds the type of the object being presisted. 
+	bool _save(BaseClass);             // saves the passed base class. 
+	BaseClass _load (string className);// loads the ClassName and returns a reference to it. 
   };
 
 #endif
